ab1a2041ddd4b1566cdafc82e575d1d199903be3
SebastienLamotte <sebastien.lamotte87@gmail.com>
2020-12-25 19:20:14 +0100

    Sunrise and sunset description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.openweathermap.org/data/2.5/weather?lang=fr&units=metric&appid=b246934521af834ba5fb062fcff8076c&lat=' + latitude + '&lon=' + longitude;
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lang=en&units=metric&appid=b246934521af834ba5fb062fcff8076c&lat=' + latitude + '&lon=' + longitude;
     
     request({url, json: true}, (error, {body}) => {
         if (error) {
@@ -9,8 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (!body.weather) {
             callback('Unable to find location', undefined);
         } else {
-            const {weather, main} = body
-            callback(undefined, weather[0].main + " it is. It's currently "+ main.temp + " degrees out. There is " + main.humidity + " % of humidity.");
+            const {weather, main, sys} = body
+            const sunrise = new Date((sys.sunrise)*1000);
+            const sunset = new Date((sys.sunset)*1000)
+            callback(undefined, weather[0].description.charAt(0).toUpperCase() + weather[0].description.slice(1) + "! It's currently "+ main.temp + " degrees out. There is " + main.humidity + " % of humidity. Sunrise : " + sunrise.getHours() + "h" + sunrise.getMinutes() + "m. And sunset: " + sunset.getHours() + "h" + sunset.getMinutes() + "m.");
         }
     })
 }
